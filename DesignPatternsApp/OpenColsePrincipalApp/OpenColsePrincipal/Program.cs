a12dcff129dbcb4b12fc59aab44b14d79345bdff
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-07-15 22:54:58 +0600

    Specification Pattern to maintain open close principal.

--- DesignPatternsApp/OpenColsePrincipalApp/OpenColsePrincipal/Program.cs
+++ DesignPatternsApp/OpenColsePrincipalApp/OpenColsePrincipal/Program.cs
@@ -69,26 +69,128 @@ namespace OpenColsePrincipal
         }
 
 
+        //specification pattern start
+        public interface ISpecification<T>
+        {
+            bool isSatisfied(T t);
+        }
+
+        public interface IFilter<T>
+        {
+            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+
+        public class ColorSpecification : ISpecification<Product>
+        {
+            private Color _color;
+
+            public ColorSpecification(Color color)
+            {
+                _color = color;
+            }
+
+            public bool isSatisfied(Product t)
+            {
+                return t.Color == _color;
+            }
+        }
+
+
+        public class SizeSpecification : ISpecification<Product>
+        {
+            private Size _size;
+
+            public SizeSpecification(Size size)
+            {
+                _size = size;
+            }
+
+            public bool isSatisfied(Product t)
+            {
+                return t.Size == _size;
+            }
+        }
+
+        public class AndSpecification<T> : ISpecification<T>
+        {
+            private ISpecification<T> _specification1, _specification2;
+            public AndSpecification(ISpecification<T> specification1, ISpecification<T> specification2)
+            {
+                _specification1 = specification1;
+                _specification2 = specification2;
+            }
+
+            public bool isSatisfied(T t)
+            {
+                return _specification1.isSatisfied(t) && _specification2.isSatisfied(t);
+            }
+        }
+
+        public class BatterFilter : IFilter<Product>
+        {
+            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+            {
+                foreach (var item in items)
+                {
+                    if (spec.isSatisfied(item))
+                        yield return item;
+                }
+            }
+        }
+
+
+        //specification pattern end
+
+
         static void Main(string[] args)
         {
             var apple = new Product("Apple", Color.Green, Size.Small);
             var tree = new Product("Tree", Color.Green, Size.Large);
-            var house = new Product("House", Color.Blue, Size.Small);
+            var house = new Product("House", Color.Blue, Size.Medium);
 
             Product[] products = { apple, tree, house };
 
             //violate  open-close pricpal start
-            var productFilter = new ProductFilter();
+            //var productFilter = new ProductFilter();
+
+            //Console.WriteLine("Only Green Products are : ");
+            //foreach (Product p in productFilter.FilterByColor(products, Color.Green))
+            //{
+            //    Console.WriteLine($"{p.Name}");
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("Only Small Products are : ");
+            //foreach (Product p in productFilter.FilterBySize(products, Size.Small))
+            //{
+            //    Console.WriteLine($"{p.Name}");
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Only Small and Green Products are : ");
+            //foreach (Product p in productFilter.FilterBySizeAndColor(products, Size.Small, Color.Green))
+            //{
+            //    Console.WriteLine($"{p.Name}");
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //violate  open-close pricpal End
+
+
+            // using pecification pattern
+            var betterFilter = new BatterFilter();
 
             Console.WriteLine("Only Green Products are : ");
-            foreach (Product p in productFilter.FilterByColor(products,Color.Green))
+            foreach (Product p in betterFilter.Filter(products, new ColorSpecification(Color.Green)))
             {
                 Console.WriteLine($"{p.Name}");
             }
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("Only Small Products are : ");
-            foreach (Product p in productFilter.FilterBySize(products, Size.Small))
+            foreach (Product p in betterFilter.Filter(products, new SizeSpecification(Size.Small
+                )))
             {
                 Console.WriteLine($"{p.Name}");
             }
@@ -96,16 +198,15 @@ namespace OpenColsePrincipal
             Console.WriteLine("");
 
             Console.WriteLine("Only Small and Green Products are : ");
-            foreach (Product p in productFilter.FilterBySizeAndColor(products, Size.Small,Color.Green))
+            foreach (Product p in betterFilter.Filter(products, new AndSpecification<Product>(new SizeSpecification(Size.Small),new ColorSpecification(Color.Green))))
             {
                 Console.WriteLine($"{p.Name}");
             }
+
             Console.WriteLine("");
             Console.WriteLine("");
-            //violate  open-close pricpal End
 
-
-
+            // using pecification pattern
         }
     }
 }
