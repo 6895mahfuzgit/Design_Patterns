bab1d24ee20456be3a601dd337d00591e308cbbb
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-08-13 10:38:57 +0600

    Rules Engine Pattern

---
+++ DesignPatternsApp/RulesEnginePatternApp/RulesEnginePatternExapmle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RulesEnginePatternExapmle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
